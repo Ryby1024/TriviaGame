d4955d80fa423106f7ac323d52c844866d971a37
Roy Davis <myroy1024@yahoo.com>
2019-10-13 19:49:39 -0400

    named variables and starting on the functionality

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+// Assigning global variables
+let rightAnswers = 0;
+let wrongAnswers = 0;
+let timer = 30;
+let count = 0;
+
+let trivia = {
+    bigBangQuestions:{
+         q1: "Which character in the Big Bang Theory does NOT have a doctorate?", 
+         q2: "What has been Sheldon's life goal?"
+    },
+    options:{ 
+         q1: ["Raj", "Howard", "Sheldon","Leonard"],
+         q2: ["To get married", "To have a child", "To win the Nobel Prize", "To learn to drive"],
+    },
+    answers: {
+         q1: "Howard",
+         q2: "To win the Nobel Prize",
+    },
+} 
+      
+
+function start() {
+    
+    
+    
+
+    }
+    
+    
+   
+    
+    
+    
+
+
