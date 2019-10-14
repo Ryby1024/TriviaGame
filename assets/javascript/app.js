1adaef7d30666ed8cc06adb65cf5eb04b62225b2
Roy Davis <myroy1024@yahoo.com>
2019-10-14 15:36:17 -0400

    got the questions iterating through

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,35 +2,78 @@
 let rightAnswers = 0;
 let wrongAnswers = 0;
 let timer = 30;
-let count = 0;
+let questionCount = 0;
 
-let trivia = {
-    bigBangQuestions:{
-         q1: "Which character in the Big Bang Theory does NOT have a doctorate?", 
-         q2: "What has been Sheldon's life goal?"
+let questions = [
+    {
+         title: "Which character in the Big Bang Theory does NOT have a doctorate?", 
+         answers: ["Raj", "Sheldon", "Howard", "Leonard"],
+         correct: 2
     },
-    options:{ 
-         q1: ["Raj", "Howard", "Sheldon","Leonard"],
-         q2: ["To get married", "To have a child", "To win the Nobel Prize", "To learn to drive"],
+    {
+         title: "What has Sheldon wanted to do his whole life",
+         answers: ["Get Married", "Have a child", "Learn to drive", "Win the Nobel Prize"],
+         correct: 3
     },
-    answers: {
-         q1: "Howard",
-         q2: "To win the Nobel Prize",
-    },
-} 
-      
+     
+];
+$(document).ready(function(){
+     $("#startgame").click(function(e){
+     $("#startgame").hide();
+     $("#quiz").show();
+     showQuestion();
+   })
+   $("#quiz ul").on("click", "li", function(){
+     $(".choice").removeClass("choice");
+     $(this).addClass("choice");
+
+   })
+  
+   $("#quiz ul").click(function(e){
+     if($("li.choice").length){
+          let guess = $("li.choice").attr("id");
+          checkAnswer(guess)
+     }else {
+          alert("Please select an answer")
+     }
+     
+     
+   });
+   
+});  
 
 function start() {
     
-    
-    
-
     }
     
-    
+function showQuestion(){
+     let ques = questions[questionCount];
+          $("#quiz h2").text(ques.title);
+          $("#quiz ul").html("");
+     for(var i = 0; i < ques.answers.length; i++){
+          $("#quiz ul").append("<li id='"+ i +"'>" + ques.answers[i]+"</li>");
+     }
+}
    
-    
-    
-    
+function checkAnswer(guess){
+     let ques = questions[questionCount];
+     if(ques.correct === guess){
+
+          $("#quiz ul").html("<img src=assets/images/howard.gif " + "Right Answer")    
+          rightAnswers++;
+     } else    {
+          $("#quiz ul").html("<img src='assets/images/howard.gif' " + "Wrong answer, the answer was Howard")  
+          wrongAnswers++;
+}
+     questionCount++;
+     setTimeout(showQuestion, 3000);    
+}
+
+     
+
+
+function showResults(){
+
+}
 
 
